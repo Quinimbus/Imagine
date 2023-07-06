d2ac340fe32381343b315faeaa1f4baa3422a035
Marcel Witte <marcel@aliquis.de>
2023-07-06 03:28:23 +0200

    added ThumbParams to control thumbnail generation

--- modules/api/src/main/java/cloud/quinimbus/imagine/api/Thumbnailer.java
+++ modules/api/src/main/java/cloud/quinimbus/imagine/api/Thumbnailer.java
@@ -6,13 +6,17 @@ import java.util.ServiceLoader;
 
 public interface Thumbnailer {
     
+    public static record ThumbParams(Integer width, Integer height) {
+        
+    }
+    
     static final Thumbnailer instance = ServiceLoader.load(Thumbnailer.class).findFirst().orElseThrow(() -> new IllegalStateException("Cannot find the quinimbus core library, is it on the classpath / modulepath?"));
 
     static Thumbnailer get() {
         return instance;
     }
     
-    byte[] createImageThumb(InputStream is) throws IOException, BinaryResolutionException;
+    byte[] createImageThumb(InputStream is, ThumbParams params) throws IOException, BinaryResolutionException;
     
-    byte[] createFontThumb(InputStream is) throws IOException, BinaryResolutionException;
+    byte[] createFontThumb(InputStream is, ThumbParams params) throws IOException, BinaryResolutionException;
 }
--- modules/core/src/main/java/cloud/quinimbus/imagine/thumb/ThumbnailerImpl.java
+++ modules/core/src/main/java/cloud/quinimbus/imagine/thumb/ThumbnailerImpl.java
@@ -10,20 +10,38 @@ import cloud.quinimbus.imagine.api.Thumbnailer;
 import cloud.quinimbus.imagine.template.ImageCreatorImpl;
 import cloud.quinimbus.imagine.template.resolve.ResolvableIntegerImpl;
 import cloud.quinimbus.imagine.template.resolve.ResolvableStringImpl;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
+import javax.imageio.ImageIO;
 
 public class ThumbnailerImpl implements Thumbnailer {
 
     @Override
-    public byte[] createImageThumb(InputStream is) throws IOException, BinaryResolutionException {
+    public byte[] createImageThumb(InputStream is, ThumbParams params) throws IOException, BinaryResolutionException {
+        var width = params.width();
+        var height = params.height();
+        InputStream imgIs;
+        if (height == null || width == null) {
+            var imgBytes = is.readAllBytes();
+            var img = ImageIO.read(new ByteArrayInputStream(imgBytes));
+            var aspectRatio = (float)img.getWidth() / img.getHeight();
+            if (params.width() == null) {
+                width = Math.round(height * aspectRatio);
+            } else {
+                height = Math.round(width / aspectRatio);
+            }
+            imgIs = new ByteArrayInputStream(imgBytes);
+        } else {
+            imgIs = is;
+        }
         var template = new ImageTemplate(
                 List.of(),
                 List.of(),
-                new Base(200, 200, "#FFFFFF"),
+                new Base(width, height, "#FFFFFF"),
                 List.of(
                         new TemplateStep(
                                 TemplateStep.Type.image,
@@ -33,21 +51,21 @@ public class ThumbnailerImpl implements Thumbnailer {
                                 null,
                                 null,
                                 null,
-                                new Position(0, 0, 200, 200),
+                                new Position(0, 0, width, height),
                                 null)
                 ));
         var creator = new ImageCreatorImpl(template);
         var bos = new ByteArrayOutputStream();
-        creator.createImage(Map.of(), Map.of(), bos, id -> is);
+        creator.createImage(Map.of(), Map.of(), bos, id -> imgIs);
         return bos.toByteArray();
     }
 
     @Override
-    public byte[] createFontThumb(InputStream is) throws IOException, BinaryResolutionException {
+    public byte[] createFontThumb(InputStream is, ThumbParams params) throws IOException, BinaryResolutionException {
         var template = new ImageTemplate(
                 List.of(),
                 List.of(),
-                new Base(100, 100, "#FFFFFF"),
+                new Base(params.width(), params.height(), "#FFFFFF"),
                 List.of(
                         new TemplateStep(
                                 TemplateStep.Type.text,
@@ -57,13 +75,13 @@ public class ThumbnailerImpl implements Thumbnailer {
                                 null,
                                 null,
                                 new cloud.quinimbus.imagine.api.TemplateStep.Textarea(
-                                        new ResolvableStringImpl("ABCDEF"),
+                                        new ResolvableStringImpl("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
                                         new ResolvableStringImpl("font"),
-                                        new ResolvableIntegerImpl(32),
+                                        new ResolvableIntegerImpl(Math.round(params.height() / 3f)),
                                         new ResolvableStringImpl("#000000"),
                                         new ResolvableStringImpl("left"),
                                         new ResolvableStringImpl("top")),
-                                new Position(0, 0, 100, 100),
+                                new Position(0, 0, params.width(), params.height()),
                                 new AutoScale(TemplateStep.ScaleMethod.none, true, true, true, true)),
                         new TemplateStep(
                                 TemplateStep.Type.text,
@@ -73,13 +91,13 @@ public class ThumbnailerImpl implements Thumbnailer {
                                 null,
                                 null,
                                 new cloud.quinimbus.imagine.api.TemplateStep.Textarea(
-                                        new ResolvableStringImpl("abcdef"),
+                                        new ResolvableStringImpl("abcdefghijklmnopqrstuvwxyz"),
                                         new ResolvableStringImpl("font"),
-                                        new ResolvableIntegerImpl(32),
+                                        new ResolvableIntegerImpl(Math.round(params.height() / 3f)),
                                         new ResolvableStringImpl("#000000"),
                                         new ResolvableStringImpl("left"),
                                         new ResolvableStringImpl("bottom")),
-                                new Position(0, 0, 100, 100),
+                                new Position(0, 0, params.width(), params.height()),
                                 new AutoScale(TemplateStep.ScaleMethod.none, true, true, true, true))
                 ));
         var creator = new ImageCreatorImpl(template);
